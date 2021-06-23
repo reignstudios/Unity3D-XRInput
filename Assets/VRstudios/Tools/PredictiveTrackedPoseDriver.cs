665d0cbabd9bb2420608828dbd0a259cefd5ce85
Andrew Witte <zezba9000@gmail.com>
2021-06-23 13:04:46 -0700

    added basic predictive tracking pose driver

---
+++ Assets/VRstudios/Tools/PredictiveTrackedPoseDriver.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+namespace VRstudios.Tools
+{
+	public class PredictiveTrackedPoseDriver : TrackedPoseDriver
+	{
+		struct PrevTransform
+		{
+			public Vector3 pos, posPredicted;
+			public Quaternion rot, rotPredicted;
+		}
+
+		private PrevTransform prevTransform;
+		private bool canUpdate;
+
+		public float linearVelClampMul = 10, linearVelMul = 1;
+		public float angularVelClampMul = .1f, angularVelMul = 1;
+
+		protected override void Awake()
+		{
+			// make sure prev rotations start as identity
+			prevTransform.rot = Quaternion.identity;
+			prevTransform.rotPredicted = Quaternion.identity;
+
+			base.Awake();
+		}
+
+		protected override void Update()
+		{
+			canUpdate = true;// only update before game-logic
+			base.Update();
+			canUpdate = false;// don't update prediction before drawing frame
+		}
+
+		protected override void SetLocalTransform(Vector3 newPosition, Quaternion newRotation, PoseDataFlags poseFlags)
+		{
+			// calculate predictions
+			if (canUpdate)// only update before render
+			{
+				if ((poseFlags & PoseDataFlags.Position) != 0)
+				{
+					// capture vel & store this pos
+					var linearVel = newPosition - prevTransform.pos;
+					prevTransform.pos = newPosition;
+
+					// scale vel
+					linearVel *= linearVelMul;
+
+					// apply vel offset strong the faster we're moving
+					newPosition = Vector3.Lerp(newPosition, newPosition + linearVel, Mathf.Min(linearVel.magnitude * linearVelClampMul, 1.0f));
+					prevTransform.posPredicted = newPosition;// capture predicted pos
+				}
+
+				if ((poseFlags & PoseDataFlags.Rotation) != 0)
+				{
+					// capture vel & store this rot
+					var angularVel = Quaternion.Inverse(newRotation) * prevTransform.rot;
+					prevTransform.rot = newRotation;
+
+					// scale vel
+					angularVel.ToAngleAxis(out float angle, out var axis);
+					angularVel = Quaternion.AngleAxis(angle * angularVelMul, axis);
+
+					// apply vel offset strong the faster we're moving
+					newRotation = Quaternion.Lerp(newRotation, angularVel * newRotation, Mathf.Min(angle * angularVelClampMul, 1.0f));
+					prevTransform.rotPredicted = newRotation;// capture predicted rot
+				}
+			}
+			else
+			{
+				// apply predicted transform to object before drawing
+				newPosition = prevTransform.posPredicted;
+				newRotation = prevTransform.rotPredicted;
+			}
+
+			// set modified transform
+			base.SetLocalTransform(newPosition, newRotation, poseFlags);
+		}
+	}
+}
