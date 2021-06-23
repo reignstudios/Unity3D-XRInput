4d328fc58d7c2a09096093eee872f4443738b4a0
Andrew Witte <zezba9000@gmail.com>
2021-06-23 15:06:13 -0700

    added more predictive options

--- Assets/VRstudios/Tools/PredictiveTrackedPoseDriver.cs
+++ Assets/VRstudios/Tools/PredictiveTrackedPoseDriver.cs
@@ -3,6 +3,12 @@ using UnityEngine.SpatialTracking;
 
 namespace VRstudios.Tools
 {
+	public enum PredictionMode
+	{
+		OnUpdate,
+		BeforeRender
+	}
+
 	public class PredictiveTrackedPoseDriver : TrackedPoseDriver
 	{
 		struct PrevTransform
@@ -14,6 +20,8 @@ namespace VRstudios.Tools
 		private PrevTransform prevTransform;
 		private bool canUpdate;
 
+		public bool enablePrediction = true;
+		public PredictionMode mode = PredictionMode.BeforeRender;
 		public float linearVelClampMul = 10, linearVelMul = 1;
 		public float angularVelClampMul = .1f, angularVelMul = 1;
 
@@ -28,50 +36,53 @@ namespace VRstudios.Tools
 
 		protected override void Update()
 		{
-			canUpdate = true;// only update before game-logic
+			canUpdate = mode == PredictionMode.OnUpdate;
 			base.Update();
-			canUpdate = false;// don't update prediction before drawing frame
+			canUpdate = !canUpdate;
 		}
 
 		protected override void SetLocalTransform(Vector3 newPosition, Quaternion newRotation, PoseDataFlags poseFlags)
 		{
 			// calculate predictions
-			if (canUpdate)// only update before render
+			if (enablePrediction)
 			{
-				if ((poseFlags & PoseDataFlags.Position) != 0)
+				if (canUpdate)// only update before render
 				{
-					// capture vel & store this pos
-					var linearVel = newPosition - prevTransform.pos;
-					prevTransform.pos = newPosition;
+					if ((poseFlags & PoseDataFlags.Position) != 0)
+					{
+						// capture vel & store this pos
+						var linearVel = newPosition - prevTransform.pos;
+						prevTransform.pos = newPosition;
 
-					// scale vel
-					linearVel *= linearVelMul;
+						// scale vel
+						linearVel *= linearVelMul;
 
-					// apply vel offset strong the faster we're moving
-					newPosition = Vector3.Lerp(newPosition, newPosition + linearVel, Mathf.Min(linearVel.magnitude * linearVelClampMul, 1.0f));
-					prevTransform.posPredicted = newPosition;// capture predicted pos
+						// apply vel offset strong the faster we're moving
+						newPosition = Vector3.Lerp(newPosition, newPosition + linearVel, Mathf.Min(linearVel.magnitude * linearVelClampMul, 1.0f));
+						prevTransform.posPredicted = newPosition;// capture predicted pos
+					}
+
+					if ((poseFlags & PoseDataFlags.Rotation) != 0)
+					{
+						// capture vel & store this rot
+						var angularVel = Quaternion.Inverse(newRotation) * prevTransform.rot;
+						prevTransform.rot = newRotation;
+
+						// scale vel
+						angularVel.ToAngleAxis(out float angle, out var axis);
+						angularVel = Quaternion.AngleAxis(angle * angularVelMul, axis);
+
+						// apply vel offset strong the faster we're moving
+						newRotation = Quaternion.Lerp(newRotation, angularVel * newRotation, Mathf.Min(angle * angularVelClampMul, 1.0f));
+						prevTransform.rotPredicted = newRotation;// capture predicted rot
+					}
 				}
-
-				if ((poseFlags & PoseDataFlags.Rotation) != 0)
+				else if (mode == PredictionMode.OnUpdate)
 				{
-					// capture vel & store this rot
-					var angularVel = Quaternion.Inverse(newRotation) * prevTransform.rot;
-					prevTransform.rot = newRotation;
-
-					// scale vel
-					angularVel.ToAngleAxis(out float angle, out var axis);
-					angularVel = Quaternion.AngleAxis(angle * angularVelMul, axis);
-
-					// apply vel offset strong the faster we're moving
-					newRotation = Quaternion.Lerp(newRotation, angularVel * newRotation, Mathf.Min(angle * angularVelClampMul, 1.0f));
-					prevTransform.rotPredicted = newRotation;// capture predicted rot
+					// apply predicted transform to object before drawing
+					newPosition = prevTransform.posPredicted;
+					newRotation = prevTransform.rotPredicted;
 				}
-			}
-			else
-			{
-				// apply predicted transform to object before drawing
-				newPosition = prevTransform.posPredicted;
-				newRotation = prevTransform.rotPredicted;
 			}
 
 			// set modified transform
